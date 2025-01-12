a492bcbffcd42cb14b3a5f72cf736a72ed902176
Youssefakq <youssefkhalifaquispe@gmail.com>
2025-01-12 05:47:51 +0100

    Añadida WEB ANALYTICS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter} from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({
   variable: "--font-inter",
@@ -25,6 +25,7 @@ export default function RootLayout({
       <body
         className={`${inter.variable} antialiased min-h-screen scroll-smooth bg-neutral-100 font-sans antialiased selection:bg-neutral-200 dark:bg-neutral-900 dark:selection:bg-neutral-700"`}
       >
+        <Analytics />
         {children}
       </body>
     </html>

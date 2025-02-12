f8b6afcda859cfc4e4a7bb81eb6acf56ee6d6f1a
Youssefakq <youssefkhalifaquispe@gmail.com>
2025-02-12 16:38:14 +0100

    Cambios de color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,32 +3,32 @@ import { Card, CardHeader, CardBody, CardFooter, Link } from '@nextui-org/react'
 
 export default function Home() {
   return (
-    <main className="relative flex min-h-screen flex-col py-4 md:py-6 items-center justify-center p-10">
+    <main className="relative flex min-h-screen flex-col py-4 md:py-6 items-center justify-center p-10 bg-slate-900 text-white">
       <div className="container max-w-3xl flex-1">
-      <nav className="container sticky top-0 z-50 mb-1 flex w-full max-w-4xl items-center justify-between bg-neutral-100/80 py-4 backdrop-blur-md dark:bg-neutral-900/80 md:mx-[-25px] md:px-[25px]">
-      <a href="https://youssefportfolio-rouge.vercel.app" className="">YoussefAKQ</a>
+        <nav className="container sticky top-0 z-50 mb-1 flex w-full max-w-4xl items-center justify-between bg-slate-900 py-4 backdrop-blur-md dark:bg-slate-900 md:mx-[-25px] md:px-[25px]">
+          <a href="https://youssefportfolio-rouge.vercel.app" className="text-white">YoussefAKQ</a>
           <div className="flex items-center space-x-3">
             <a
               href="https://github.com/YoussefAKQ"
-              className="hover:bg-neutral-200 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50"
+              className="hover:bg-slate-800 hover:text-neutral-900 dark:hover:bg-slate-700 dark:hover:text-neutral-50"
             >
               <FaGithub className="text-2xl" />
             </a>
             <a
               href="https://www.linkedin.com/in/youssef-alexander-khalifa-quispe-622a6b2bb/"
-              className="hover:bg-neutral-200 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50"
+              className="hover:bg-slate-800 hover:text-neutral-900 dark:hover:bg-slate-700 dark:hover:text-neutral-50"
             >
               <FaLinkedin className="text-2xl" />
             </a>
             <a
               href="https://www.instagram.com/youssefakq/"
-              className="hover:bg-neutral-200 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50"
+              className="hover:bg-slate-800 hover:text-neutral-900 dark:hover:bg-slate-700 dark:hover:text-neutral-50"
             >
               <FaInstagram className="text-2xl" />
             </a>
             <a
               href="https://x.com/YoussefAKQ"
-              className="hover:bg-neutral-200 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50"
+              className="hover:bg-slate-800 hover:text-neutral-900 dark:hover:bg-slate-700 dark:hover:text-neutral-50"
             >
               <FaTwitter className="text-2xl" />
             </a>
@@ -46,10 +46,10 @@ export default function Home() {
           <br />
           <b className="text-xl">Proyectos</b>
           <div className="grid grid-cols-1 gap-3 md:grid-cols-2">
-            <div className="relative flex flex-col rounded-md border-[1px] border-neutral-300 px-2 py-2 shadow-sm dark:border-neutral-800 hover:bg-neutral-200 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50">
+            <div className="relative flex flex-col rounded-md border-[1px] border-neutral-300 px-2 py-2 shadow-sm dark:border-neutral-800 hover:bg-slate-800 hover:text-neutral-900 dark:hover:bg-slate-700 dark:hover:text-neutral-50">
               <Card className="max-w-[400px] text-sm ">
                 <CardHeader className="flex gap-2 p-1">
-                <FaShoppingCart className="text-2xl" />
+                  <FaShoppingCart className="text-2xl" />
                   <div className="flex flex-col">
                     <Link
                       isExternal
@@ -64,7 +64,7 @@ export default function Home() {
 
                 <CardBody className="p-2 text-xs sm:text-sm">
                   <p>
-                  SmartCart: App para planificar compras, encontrar opciones económicas y recibir recomendaciones de recetas (React Native).
+                    SmartCart: App para planificar compras, encontrar opciones económicas y recibir recomendaciones de recetas (React Native).
                   </p>
                 </CardBody>
 
@@ -73,53 +73,54 @@ export default function Home() {
                 </CardFooter>
               </Card>
             </div>
-            
           </div>
         </div>
+
         <div className="container max-w-4xl flex-1 flex flex-col space-y-4 delay-100 duration-500 animate-in fade-in slide-in-from-bottom-4 fill-mode-backwards">
           <br />
-          <b className="text-xl">Skills princpales</b>
+          <b className="text-xl">Skills principales</b>
           <div className="grid grid-cols-2 gap-3 md:grid-cols-3 ">
-          <div className="text-xs md:text-sm text-sm items-center justify-center relative flex flex-col rounded-md border-[1px] border-neutral-300 px-2 py-2 shadow-sm dark:border-neutral-800 hover:bg-neutral-200 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50">
+            <div className="text-xs md:text-sm text-sm items-center justify-center relative flex flex-col rounded-md border-[1px] border-neutral-300 px-2 py-2 shadow-sm dark:border-neutral-800 hover:bg-slate-800 hover:text-neutral-900 dark:hover:bg-slate-700 dark:hover:text-neutral-50">
               Java
             </div>
-            <div className="text-xs md:text-sm text-sm items-center justify-center relative flex flex-col rounded-md border-[1px] border-neutral-300 px-2 py-2 shadow-sm dark:border-neutral-800 hover:bg-neutral-200 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50">
+            <div className="text-xs md:text-sm text-sm items-center justify-center relative flex flex-col rounded-md border-[1px] border-neutral-300 px-2 py-2 shadow-sm dark:border-neutral-800 hover:bg-slate-800 hover:text-neutral-900 dark:hover:bg-slate-700 dark:hover:text-neutral-50">
               Python
             </div>
-            <div className="text-xs md:text-sm text-sm items-center justify-center relative flex flex-col rounded-md border-[1px] border-neutral-300 px-2 py-2 shadow-sm dark:border-neutral-800 hover:bg-neutral-200 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50">
+            <div className="text-xs md:text-sm text-sm items-center justify-center relative flex flex-col rounded-md border-[1px] border-neutral-300 px-2 py-2 shadow-sm dark:border-neutral-800 hover:bg-slate-800 hover:text-neutral-900 dark:hover:bg-slate-700 dark:hover:text-neutral-50">
               MySQL
             </div>
-            <div className="text-xs md:text-sm text-sm items-center justify-center relative flex flex-col rounded-md border-[1px] border-neutral-300 px-2 py-2 shadow-sm dark:border-neutral-800 hover:bg-neutral-200 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50">
+            <div className="text-xs md:text-sm text-sm items-center justify-center relative flex flex-col rounded-md border-[1px] border-neutral-300 px-2 py-2 shadow-sm dark:border-neutral-800 hover:bg-slate-800 hover:text-neutral-900 dark:hover:bg-slate-700 dark:hover:text-neutral-50">
               HTML
             </div>
-            <div className="text-xs md:text-sm text-sm items-center justify-center relative flex flex-col rounded-md border-[1px] border-neutral-300 px-2 py-2 shadow-sm dark:border-neutral-800 hover:bg-neutral-200 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50">
+            <div className="text-xs md:text-sm text-sm items-center justify-center relative flex flex-col rounded-md border-[1px] border-neutral-300 px-2 py-2 shadow-sm dark:border-neutral-800 hover:bg-slate-800 hover:text-neutral-900 dark:hover:bg-slate-700 dark:hover:text-neutral-50">
               CSS
             </div>
-            <div className="text-xs md:text-sm text-sm items-center justify-center relative flex flex-col rounded-md border-[1px] border-neutral-300 px-2 py-2 shadow-sm dark:border-neutral-800 hover:bg-neutral-200 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50">
+            <div className="text-xs md:text-sm text-sm items-center justify-center relative flex flex-col rounded-md border-[1px] border-neutral-300 px-2 py-2 shadow-sm dark:border-neutral-800 hover:bg-slate-800 hover:text-neutral-900 dark:hover:bg-slate-700 dark:hover:text-neutral-50">
               Git
             </div>
           </div>
         </div>
+
         <div className="container max-w-4xl flex-1 flex flex-col space-y-4 delay-100 duration-500 animate-in fade-in slide-in-from-bottom-4 fill-mode-backwards">
           <br />
           <b className="text-xl">Skills en aprendizaje</b>
           <div className="grid grid-cols-2 gap-3 md:grid-cols-3 ">
-            <div className="text-xs md:text-sm text-sm items-center justify-center relative flex flex-col rounded-md border-[1px] border-neutral-300 px-2 py-2 shadow-sm dark:border-neutral-800 hover:bg-neutral-200 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50">
+            <div className="text-xs md:text-sm text-sm items-center justify-center relative flex flex-col rounded-md border-[1px] border-neutral-300 px-2 py-2 shadow-sm dark:border-neutral-800 hover:bg-slate-800 hover:text-neutral-900 dark:hover:bg-slate-700 dark:hover:text-neutral-50">
               React Native
             </div>
-            <div className="text-xs md:text-sm text-sm items-center justify-center relative flex flex-col rounded-md border-[1px] border-neutral-300 px-2 py-2 shadow-sm dark:border-neutral-800 hover:bg-neutral-200 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50">
+            <div className="text-xs md:text-sm text-sm items-center justify-center relative flex flex-col rounded-md border-[1px] border-neutral-300 px-2 py-2 shadow-sm dark:border-neutral-800 hover:bg-slate-800 hover:text-neutral-900 dark:hover:bg-slate-700 dark:hover:text-neutral-50">
               Visual Basic .NET
             </div>
-            <div className="text-xs md:text-sm text-sm items-center justify-center relative flex flex-col rounded-md border-[1px] border-neutral-300 px-2 py-2 shadow-sm dark:border-neutral-800 hover:bg-neutral-200 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50">
+            <div className="text-xs md:text-sm text-sm items-center justify-center relative flex flex-col rounded-md border-[1px] border-neutral-300 px-2 py-2 shadow-sm dark:border-neutral-800 hover:bg-slate-800 hover:text-neutral-900 dark:hover:bg-slate-700 dark:hover:text-neutral-50">
               Android Studio Java
             </div>
-            <div className="text-xs md:text-sm text-sm items-center justify-center relative flex flex-col rounded-md border-[1px] border-neutral-300 px-2 py-2 shadow-sm dark:border-neutral-800 hover:bg-neutral-200 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50">
+            <div className="text-xs md:text-sm text-sm items-center justify-center relative flex flex-col rounded-md border-[1px] border-neutral-300 px-2 py-2 shadow-sm dark:border-neutral-800 hover:bg-slate-800 hover:text-neutral-900 dark:hover:bg-slate-700 dark:hover:text-neutral-50">
               JavaScript
             </div>
-            <div className="text-xs md:text-sm text-sm items-center justify-center relative flex flex-col rounded-md border-[1px] border-neutral-300 px-2 py-2 shadow-sm dark:border-neutral-800 hover:bg-neutral-200 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50">
+            <div className="text-xs md:text-sm text-sm items-center justify-center relative flex flex-col rounded-md border-[1px] border-neutral-300 px-2 py-2 shadow-sm dark:border-neutral-800 hover:bg-slate-800 hover:text-neutral-900 dark:hover:bg-slate-700 dark:hover:text-neutral-50">
               NextJS
             </div>
-            <div className="text-xs md:text-sm text-sm items-center justify-center relative flex flex-col rounded-md border-[1px] border-neutral-300 px-2 py-2 shadow-sm dark:border-neutral-800 hover:bg-neutral-200 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50">
+            <div className="text-xs md:text-sm text-sm items-center justify-center relative flex flex-col rounded-md border-[1px] border-neutral-300 px-2 py-2 shadow-sm dark:border-neutral-800 hover:bg-slate-800 hover:text-neutral-900 dark:hover:bg-slate-700 dark:hover:text-neutral-50">
               Tailwind
             </div>
           </div>
@@ -127,9 +128,9 @@ export default function Home() {
       </div>
       <footer className="container mt-10 flex items-center max-w-4xl flex-col items-center justify-center space-y-1 md:flex-col md:items-center md:justify-center md:space-y-1">
         <div className="flex items-center space-x-1 text-sm md:text-md">
-            <h2>YoussefAKQ</h2>
-            <span className="text-gray-500 dark:text-gray-400">-</span>
-            <p>2025</p>
+          <h2>YoussefAKQ</h2>
+          <span className="text-gray-500 dark:text-gray-400">-</span>
+          <p>2025</p>
         </div>
       </footer>
     </main>

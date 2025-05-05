be96d40f0eb397fb1a1309987cff7db0b7706fc7
Youssef Khalifa Quispe <youssef.khalifa@atisa.es>
2025-05-05 17:20:21 +0200

    Mejora en imágenes

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,11 +1,13 @@
 const ProjectItem = ({ title, description, image, skills, link }) => {
   return (
     <div className='group flex flex-row gap-4 hover:bg-white/10 p-4 rounded-lg transition duration-125 ease-in-out'>
-      <img 
-        src={image} 
-        alt={title} 
-        className="w-32 md:w-40 h-20 md:h-28 object-cover rounded-xl border-2 border-slate-200/10 transition group-hover:border-slate-200/30 flex-shrink-0" 
-      />
+      <div className="w-32 md:w-40 h-24 md:h-32 flex-shrink-0">
+        <img 
+          src={image} 
+          alt={title} 
+          className="md:-mt-4 -mt-2 w-full h-full object-contain rounded-2xl border-2 border-slate-200/10 transition group-hover:border-slate-200/30" 
+        />
+      </div>
       <div className='space-y-2 flex-grow min-w-0'>
         <h3 className='text-lg md:text-xl font-bold -mt-1'>{title}</h3>
         <p className='text-sm md:text-base'>{description}</p>

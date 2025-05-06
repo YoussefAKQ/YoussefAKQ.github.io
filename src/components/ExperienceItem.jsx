9e94c9e0b757e61d9e285e1c9a659dcbe354e3a1
Youssef Khalifa Quispe <youssef.khalifa@atisa.es>
2025-05-06 11:08:15 +0200

    Pequeñas mejoras

--- src/components/ExperienceItem.jsx
+++ src/components/ExperienceItem.jsx
@@ -1,13 +1,13 @@
 const ExperienceItem = ({ date, title, description, skills }) => {
   return (
-    <div className='flex flex-row gap-6 hover:bg-white/10 p-3 rounded-lg transition duration-125 ease-in-out md:flex-row flex-col'>
+    <div className='flex flex-row gap-6 hover:bg-white/10 p-3 rounded-lg transition duration-125 ease-in-out md:flex-row flex-col cursor-default'>
       <p className='text-gray-400 text-xs'>{date}</p>
       <div className='max-w-[450px] space-y-2 text-sm md:text-base'>
         <p className='text-xl font-bold -mt-1'>{title}</p>
         <p>{description}</p>
         <div className='flex flex-wrap gap-2 mt-2'>
           {skills.map((skill, index) => (
-            <p key={index} className='text-xs md:text-sm text-gray-500 bg-white/5 px-2 py-1 rounded '>{skill}</p>
+            <p key={index} className='text-xs md:text-sm text-gray-500 bg-white/5 px-2 py-1 rounded cursor-default'>{skill}</p>
           ))}
         </div>
       </div>
--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,6 +1,6 @@
 const ProjectItem = ({ title, description, image, skills, link }) => {
   return (
-    <div className='group flex flex-row gap-4 hover:bg-white/10 p-4 rounded-lg transition duration-125 ease-in-out'>
+    <div className='group flex flex-row gap-4 hover:bg-white/10 p-4 rounded-lg transition duration-125 ease-in-out cursor-default'>
       <div className="w-32 md:w-40 h-24 md:h-32 flex-shrink-0">
         <img 
           src={image} 
@@ -13,7 +13,7 @@ const ProjectItem = ({ title, description, image, skills, link }) => {
         <p className='text-sm md:text-base'>{description}</p>
         <div className='flex flex-wrap gap-2 mt-2'>
           {skills.map((skill, index) => (
-            <span key={index} className='text-xs md:text-sm text-gray-500 bg-white/5 px-2 py-1 rounded'>{skill}</span>
+            <span key={index} className='text-xs md:text-sm text-gray-500 bg-white/5 px-2 py-1 rounded cursor-default'>{skill}</span>
           ))}
         </div>
         {link && (

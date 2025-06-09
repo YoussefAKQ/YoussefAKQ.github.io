551cb798446bc0126037aebf0ddfd1cbc3227296
Youssefakq <youssefkhalifaquispe@gmail.com>
2025-06-09 23:36:21 +0200

    Añadida carta de recomendación

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,15 +4,30 @@ const Experience = () => {
   const experiences = [
     {
       date: "March 2025 - June 2025",
-      title: "Junior Developer Intern at Atisa",
-      description: "Worked on various projects involving webdevelopment, software development, and database management. Gained experience in collaborating with a team and using version control systems.",
-      skills: ["React", "Python AI Oriented", "JavaScript", "TypeScript", "Flutter", "TailwindCSS", "PHP", "HTML", "CSS", "NodeJS", "MySQL", "Git"]
+      title: "Junior Developer at Atisa",
+      description:
+        "Worked on various projects involving web development, software development, and database management. Gained experience in collaborating with a team and using version control systems.",
+      skills: [
+        "React",
+        "Python AI Oriented",
+        "JavaScript",
+        "TypeScript",
+        "Flutter",
+        "TailwindCSS",
+        "PHP",
+        "HTML",
+        "CSS",
+        "NodeJS",
+        "MySQL",
+        "Git"
+      ],
+      recommendationUrl: "./public/RLYoussef.pdf"
     }
   ];
 
   return (
     <section id='experience' className='space-y-6 mt-20 md:mt-32'>
-      <div className='flex flex-col gap-5 space-y-6 '>
+      <div className='flex flex-col gap-5 space-y-6'>
         {experiences.map((experience, index) => (
           <ExperienceItem key={index} {...experience} />
         ))}
--- src/components/ExperienceItem.jsx
+++ src/components/ExperienceItem.jsx
@@ -1,13 +1,29 @@
-const ExperienceItem = ({ date, title, description, skills }) => {
+const ExperienceItem = ({ date, title, description, skills, recommendationUrl }) => {
   return (
     <div className='flex flex-row gap-6 hover:bg-white/10 p-3 rounded-lg transition duration-125 ease-in-out md:flex-row flex-col cursor-default'>
       <p className='text-gray-400 text-xs'>{date}</p>
       <div className='max-w-[450px] space-y-2 text-sm md:text-base'>
-        <p className='text-xl font-bold -mt-1'>{title}</p>
+        <div className='flex items-center gap-2'>
+          <p className='text-xl font-bold -mt-1'>{title}</p>
+          {recommendationUrl && (
+            <a
+              href={recommendationUrl}
+              download
+              className='text-xs bg-gray-500 hover:bg-white/10 text-white px-2 py-1 rounded transition'
+            >
+              Recomendation Letter
+            </a>
+          )}
+        </div>
         <p>{description}</p>
         <div className='flex flex-wrap gap-2 mt-2'>
           {skills.map((skill, index) => (
-            <p key={index} className='text-xs md:text-sm text-gray-500 bg-white/5 px-2 py-1 rounded cursor-default'>{skill}</p>
+            <p
+              key={index}
+              className='text-xs md:text-sm text-gray-500 bg-white/5 px-2 py-1 rounded cursor-default'
+            >
+              {skill}
+            </p>
           ))}
         </div>
       </div>
